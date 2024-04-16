0fc882a21052e0d9a34a29c347a0253a5ae22768
Samuele Caccavelli <samuele.caccavelli@mail.polimi.it>
2024-04-16 14:17:52 +0000

    Implemented first skeleton of class Matrix

---
+++ src/Matrix.hpp
@@ -0,0 +1,29 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+enum StorageOrder {RowWise = 0, ColumnWise = 1};
+
+namespace algebra {
+    template<class T,    StorageOrder ORDERING>
+    class Matrix {
+        public:
+            using KeyType = std::array<std::size_t,2>;
+            using DataType = T;
+
+        private:
+            std::map<KeyType, DataType> data;
+            bool compressed;
+
+        public:
+            bool is_compressed();
+    };
+
+    template<class T, StorageOrder ORDERING>
+    bool
+    Matrix<T, ORDERING>::is_compressed() {
+        return compressed;
+    }
+
+} // namespace algebra
+
+#endif /* MATRIX_H */
